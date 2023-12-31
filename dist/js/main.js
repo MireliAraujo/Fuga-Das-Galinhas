637f52b4df37330f10748bb20fa6c2b16c8dbfdb
Mireli Araujo <mirellypollocks@gmail.com>
2023-12-31 10:57:04 -0300

    atualização de css e conteúdo

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){const e=document.querySelector(".hero").clientHeight;window.addEventListener("scroll",function(){window.scrollY<e?header.classList.add("header--is-hidden"):header.classList.remove("header--is-hidden")})});
